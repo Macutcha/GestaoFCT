0cc508cbef145e146d067ec1e4fc14a8d76e82ba
Macutcha <manuelbindo@gmail.com>
2021-04-07 19:50:26 +0200

    Update requisitar_material.php

--- core/views/requisitar_material.php
+++ core/views/requisitar_material.php
@@ -9,63 +9,64 @@ $causas = Causa::listar_causa();
 
 ?>
 
-<main>
-    <table>
-        <tr>
-            <th>id</th>
-            <th>Material</th>
-            <th>Referenci</th>
-            <th>quantidade</th>
-            <th>Selecionar Equipamento</th>
-        </tr>
-        <?php foreach ($materias as $material) : ?>
-            <tr>
-                <td> <?= $material->id_material ?> </td>
-                <td> <?= $material->tipo_material ?> </td>
-                <td> <?= $material->referencia_material ?> </td>
-                <td> <?= $material->quantidade ?> </td>
-                <td> <input type="checkbox" name="" id=""></td>
-            </tr>
-        <?php endforeach; ?>
-
-
-    </table>
-    <form action="?a=sub_requisicao" method="post">
-
+<main class="Main">
+    <section>
         <table>
             <tr>
-                <th>Equipamento</th>
-                <th>Marca</th>
-                <th>Quantidade</th>
-                <th>Motivo</th>
+                <th>id</th>
+                <th>Material</th>
+                <th>Referenci</th>
+                <th>quantidade</th>
+                <th>Selecionar Equipamento</th>
             </tr>
-            <tr>
-                <td>
-                    <select name="Equipamento" id="">
-                        <option value="<?= $material->id_material ?>"><?= $material->tipo_material ?></option>
-                    </select>
-                </td>
-                <td>
-                    <select name="Marca_Equipamento" id="">
-                        <option value=""> <?= $material->marca ?> </option>
-                    </select>
-                </td>
-                <td><input type="number" name="Quant_Equipamento" id="" placeholder="Quantidade"></td>
-                <td>
-                    <select name="Causa_Equipamento" id="id_causa">
-                        <?php foreach ($causas as $causa) : ?>
-                            <option value=""> <?= $causa->causa ?> </option>
-                        <?php endforeach; ?>
-                    </select>
-                </td>
-            </tr>
+            <?php foreach ($materias as $material) : ?>
+                <tr>
+                    <td> <?= $material->id_material ?> </td>
+                    <td> <?= $material->tipo_material ?> </td>
+                    <td> <?= $material->referencia_material ?> </td>
+                    <td> <?= $material->quantidade ?> </td>
+                    <td> <input type="checkbox" name="" id=""></td>
+                </tr>
+            <?php endforeach; ?>
+
+
         </table>
-        <!--  -->
+        <form action="?a=sub_requisicao" method="post">
 
-        <label for="id_causa">Motivo</label>
+            <table>
+                <tr>
+                    <th>Equipamento</th>
+                    <th>Marca</th>
+                    <th>Quantidade</th>
+                    <th>Motivo</th>
+                </tr>
+                <tr>
+                    <td>
+                        <select name="Equipamento" id="">
+                            <option value="<?= $material->id_material ?>"><?= $material->tipo_material ?></option>
+                        </select>
+                    </td>
+                    <td>
+                        <select name="Marca_Equipamento" id="">
+                            <option value=""> <?= $material->marca ?> </option>
+                        </select>
+                    </td>
+                    <td><input type="number" name="Quant_Equipamento" id="" placeholder="Quantidade"></td>
+                    <td>
+                        <select name="Causa_Equipamento" id="id_causa">
+                            <?php foreach ($causas as $causa) : ?>
+                                <option value=""> <?= $causa->causa ?> </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <!--  -->
 
-        <button>Requisitar</button>
-    </form>
+            <label for="id_causa">Motivo</label>
 
+            <button>Requisitar</button>
+        </form>
+    </section>
 
 </main>
