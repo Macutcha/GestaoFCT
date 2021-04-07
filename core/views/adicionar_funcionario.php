93b40eafa8962020b029c70c452286a05dded0af
Macutcha <manuelbindo@gmail.com>
2021-04-07 23:01:37 +0200

    Update adicionar_funcionario.php

--- core/views/adicionar_funcionario.php
+++ core/views/adicionar_funcionario.php
@@ -5,32 +5,35 @@ use core\models\Departamento;
 $cargos = Departamento::listar_cargo();
 ?>
 <main class="Main">
-    <h1>Adicionar Funcionario</h1>
-    <form action="?a=adicionar_funcionario" method="post">
-        <div>
-            <?php if (isset($_SESSION['erro'])) : ?>
-                <?= $_SESSION['erro'] ?>
-                <?php unset($_SESSION['erro']); ?>
-            <?php endif; ?>
+    <section>
 
-        </div>
+        <h1>Adicionar Funcionario</h1>
+        <form action="?a=adicionar_funcionario" method="post">
+            <div>
+                <?php if (isset($_SESSION['erro'])) : ?>
+                    <?= $_SESSION['erro'] ?>
+                    <?php unset($_SESSION['erro']); ?>
+                <?php endif; ?>
 
-        <!--  -->
-        <label for="nome_funcionario"> Nome do Funcionario</label>
-        <input type="text" id="nome_funcionario" name="nomeFuncionario" placeholder="Nome do funcionario">
+            </div>
 
-        <!--  -->
-        <label for="">Cargo</label>
-        <select name="cargo" id="">
-            <option value="Docente">Cargos</option>
-            <?php foreach ($cargos as $cargo) : ?>
-                <option value=" <?= $cargo->cargo ?> "> <?= $cargo->cargo ?> </option>
-            <?php endforeach; ?>
-        </select>
+            <!--  -->
+            <label for="nome_funcionario"> Nome do Funcionario</label>
+            <input type="text" id="nome_funcionario" name="nomeFuncionario" placeholder="Nome do funcionario">
 
-        <!--  -->
-        <button> Add Funcionario</button>
+            <!--  -->
+            <label for="">Cargo</label>
+            <select name="cargo" id="">
+                <option value="Docente">Cargos</option>
+                <?php foreach ($cargos as $cargo) : ?>
+                    <option value=" <?= $cargo->cargo ?> "> <?= $cargo->cargo ?> </option>
+                <?php endforeach; ?>
+            </select>
 
-    </form>
+            <!--  -->
+            <button> Add Funcionario</button>
+
+        </form>
+    </section>
 
 </main>
