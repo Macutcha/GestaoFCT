fe6a9666cf3f967ff3f1444836a30c73cf7aaf0a
Macutcha <manuelbindo@gmail.com>
2021-04-07 18:15:11 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Databases
+define("MYSQL_SERVER",                      "localhost");
+define("MYSQL_DATABASES",                   "gestao_fct");
+define("MYSQL_USER",                        "root");
+define("MYSQL_PASS",                        "");
+define("MYSQL_CHARSET",                     "utf8");
+
+// Rotas
+define("Base_URL",                            "http://localhost/gestao/public/");
